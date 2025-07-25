9841f32639ebccc8a54acb5f32bd372bb6083141
Kumar Manthri <153302220+manthri-kumar@users.noreply.github.com>
2025-07-25 19:34:01 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  restrictMode: true,
 };
-
+module.exports = nextConfig;
 export default nextConfig;
